c56787b5d99c86a0d7a5f2818bf0ee37e20d47f9
fullfool <cyq_2210@163.com>
2015-03-30 15:31:45 +0800

    nothing

--- scrapy4huafen/scrapy4huafenlinks.py
+++ scrapy4huafen/scrapy4huafenlinks.py
@@ -74,27 +74,52 @@ def writeToLog(content):
     writeLock.release()
 
 
-class DownloadOnePagePeople(threading.Thread):
+class DownloadOnePageTheme(threading.Thread):
     #三个参数分别为start，eachlen，totallen
     def __init__(self, themeUrl):
         threading.Thread.__init__(self)
         self.themeUrl = themeUrl
 
     def run(self):
-        url = self.themeUrl
+        url = 'http://cn.club.vmall.com/' + self.themeUrl
         #<a href="thread-3564472-1-2.html" onclick="atarget(this)" class="s xst">我的快乐，与您分享！</a>
         try:
             oneThemePage = urllib2.urlopen(url).read()
         except Exception as ep:
             writeToLog('cannot open page for one theme,%s' % url)
             return
+        print 'successfull get theme,%s' % url
         #<a href="space-uid-8994548.html" target="_blank" class="xi2">远离人山人海  匹配出来的第0个就是坐作者
         authorIdPattern = re.compile(r'<a href="space-uid-(\d+)\.html" target="_blank" class="xi2">')
         try:
             authorId = authorIdPattern.findall(oneThemePage)[0]
         except Exception as ep:
             writeToLog('cannot find author id info,%s' % url)
+            return
         
+        allPicsLinkPattern = re.compile(r'window\.shareUrl = "([^"]+?)"')
+        try:
+            allPicsListString = allPicsLinkPattern.findall(oneThemePage)[0]
+        except Exception as ep:
+            writeToLog('cannot find pics in theme,%s' % url)
+            return
+
+        allPicsList = allPicsListString.split('||')
+        print 'there are %s pics found in theme,%s' % (len(allPicsList),url)
+        if len(allPicsList) > 0:
+            makeDirLock.acquire()
+
+            if not os.path.exists(authorId):
+                os.makedirs(authorId)
+            filePath = os.path.join(authorId, 'picsUrlList.txt')
+            filehandler = open(filePath, 'a')
+            for eachPicUrl in allPicsList:
+                filehandler.write(eachPicUrl+'\n')
+            filehandler.close()
+
+            makeDirLock.release()
+
+        print 'successfully write pics url in theme,%s' % url
 
 
 
@@ -112,14 +137,82 @@ def testThread(url):
     except Exception as ep:
         writeToLog('cannot find author id info,%s' % url)
     #print 'authorId is %s' % authorId
+    allPicsLinkPattern = re.compile(r'window\.shareUrl = "([^"]+?)"')
+    try:
+        allPicsListString = allPicsLinkPattern.findall(oneThemePage)[0]
+    except Exception as ep:
+        writeToLog('cannot find pics in theme,%s' % url)
+        return
+
+    allPicsList = allPicsListString.split('||')
+    if len(allPicsList) > 0:
+        makeDirLock.acquire()
+
+        if not os.path.exists(authorId):
+            os.makedirs(authorId)
+        filePath = os.path.join(authorId, 'picsUrlList.txt')
+        filehandler = open(filePath, 'a')
+        for eachPicUrl in allPicsList:
+            filehandler.write(eachPicUrl+'\n')
+        filehandler.close()
+
+        makeDirLock.release()
+        
+
+
+#testThread('http://cn.club.vmall.com/thread-3558482-1-1.html')
+#sys.exit()
 
 
 
+#花粉链接一直从 http://cn.club.vmall.com/forum-141-1.html 到
+#            http://cn.club.vmall.com/forum-141-2000.html
 
+# mainPage = urllib2.urlopen('http://cn.club.vmall.com/forum-141-1234.html').read()
+# print mainPage
+# sys.exit()
 
-
-threadNum = 1
+threadNum = 100
 threadNumPool = {}
+
+startPoint = loadProcess()
+for i in range(startPoint, 2000+1):
+    setProcess(str(i))
+    print 'processing page %s' % i
+    
+    pageUrl = 'http://cn.club.vmall.com/forum-141-%s.html' % i
+    try:
+        mainPage = urllib2.urlopen(pageUrl).read()
+    except Exception as ep:
+        print ep.message
+        writeToLog('cannot open page,%s' % pageUrl)
+        continue
+
+    themePageUrlPattern = re.compile(r'<a href="(.+?)" onclick="atarget\(this\)"')
+    themePageUrlList = themePageUrlPattern.findall(mainPage)
+    print '%s themes found in page %s' % (len(themePageUrlList), i)
+    for themePageUrl in themePageUrlList:
+        findThread = False
+        while findThread == False:
+            for j in range(threadNum):
+                if not threadNumPool.has_key(j):
+                    threadNumPool[j] = DownloadOnePageTheme(themePageUrl)
+                    threadNumPool[j].start()
+                    findThread = True
+                    break
+                else:
+                    if not threadNumPool[j].isAlive():
+                        threadNumPool[j] = DownloadOnePageTheme(themePageUrl)
+                        threadNumPool[j].start()
+                        findThread = True
+                        break
+            if findThread == False: 
+                time.sleep(5)
+
+    #print themePageUrlList
+    #print len(themePageUrlList)
+
+sys.exit()
 
 
 startPage = loadProcess()
---
+++ scrapy4huafen/testdownload.py
@@ -0,0 +1,2 @@
+import urllib
+urllib.urlretrieve('http://huafans.dbankcloud.com/pic/a4439cfae11164bb1bdfa8bfd3aaa383_IMG_20150213_112948.jpg?mode=open', 'a.jpg')

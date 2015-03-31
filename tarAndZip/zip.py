e96c97630ff9822567f74d74092639c8d0cb6152
fullfool <cyq_2210@163.com>
2015-03-31 14:31:28 +0800

    update zip

--- tarAndZip/zip.py
+++ tarAndZip/zip.py
@@ -5,34 +5,44 @@ import shutil
 from zipfile import *
 import zipfile
 import threading
+import sys
 
 
 
 
-class ZipFile(threading.Thread):
+class ZipDir(threading.Thread):
 	def __init__(self, dirName):
 		threading.Thread.__init__(self)
 		self.dirName = dirName
 
 	def run(self):
+
 		dirName = self.dirName
-		f = zipfile.ZipFile('%s.zip' % dirName,'w',zipfile.ZIP_STORED)  
+		#print 'dirName is %s' % dirName
 		if os.path.exists('%s.zip' % dirName):
-			print '%s.zip has already exist' 
+			print '%s.zip has already exist\n' 
 			return
+		f = zipfile.ZipFile('%s.zip' % dirName,'w',zipfile.ZIP_STORED, allowZip64=True)  
 		startdir = dirName
 		for dirpath, dirnames, filenames in os.walk(startdir):    
 			for filename in filenames:    
 				f.write(os.path.join(dirpath,filename))    
 		f.close()
-		print 'dirName has been done'
+		print '%s has been done' % dirName
 
 
 
-for eachDir in os.listdir('.'):
+
+DirList = []
+OriList = os.listdir('.')
+for eachFile in OriList:
+	if os.path.isdir(eachFile):
+		DirList.append(eachFile)
+
+for eachDir in DirList:
 	if not os.path.isdir(eachDir):
 		continue
-	ZipFile(eachDir).start()
+	ZipDir(eachDir).start()
 
 
 
